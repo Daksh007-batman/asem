97aa89ee5668425908b25649dd6bb0b9e7cd2277
dakshkhatri007 <dakshkhatri6@gmail.com>
2025-06-19 12:51:48 +0530

    Initial Commit

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import User
-from .models import Site, HomeGatewayId, AisleGroup
+from .models import Site, HomeGatewayId, AisleGroup, aisle_blocks
 
 
 admin.site.register(User)
 admin.site.register(Site)
 admin.site.register(HomeGatewayId)
 admin.site.register(AisleGroup)
+admin.site.register(aisle_blocks)
